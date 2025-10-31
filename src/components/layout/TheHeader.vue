f6317929a5b0785a24a610b3a3d5ce9058c74148
Carlosbarbosa <carloshenrique29.qb@gmail.com>
2025-10-30 21:20:18 -0300

    update: melhora a aparência do cabeçalho com novo logotipo

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -2,8 +2,8 @@
   <header class="w-full bg-white shadow-sm px-6 py-4">
     <div class="lg:col-span-1">
        <div class="flex items-center gap-2 mb-4">
-        <img src="@/assets/img/logo.png" alt="logo" class="w-6 h-6" />
-        <span class="text-xl font-semibold text-gray-700">FocusList</span>
+        <img src="@/assets/img/logo.png" alt="logo" class="w-12 h-12" />
+        <span class="text-3xl font-semibold text-black-700">Focus</span> <span class="text-3xl font-semibold text-blue-700">List</span>
       </div>
       <StatusCard />
     </div>
