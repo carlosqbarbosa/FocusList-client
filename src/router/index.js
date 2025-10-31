1912c3d7eb26ac47e52ad708438fb313bee14662
Carlosbarbosa <carloshenrique29.qb@gmail.com>
2025-10-31 09:49:47 -0300

    update: melhora do visual da tela de pomodoro e correção de rotas

--- src/router/index.js
+++ src/router/index.js
@@ -1,43 +1,40 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-// Importação das views (telas)
 import Login from '../views/Auth/Login.vue'
 import Register from '../views/Auth/Register.vue'
 import Dashboard from '../views/Dashboard.vue'
-import Pomodoro from '../views/Pomodoro.vue'
+import PomodoroPage from '../views/PomodoroPage.vue'
 
-// Definição das rotas
 const routes = [
   {
     path: '/',
-    redirect: '/login' // Redireciona para tela de login ao acessar "/"
+    redirect: '/login',
   },
   {
     path: '/login',
     name: 'Login',
-    component: Login
+    component: Login,
   },
   {
     path: '/register',
     name: 'Register',
-    component: Register
+    component: Register,
   },
   {
     path: '/dashboard',
     name: 'Dashboard',
-    component: Dashboard
+    component: Dashboard,
   },
   {
     path: '/pomodoro',
-    name: 'Pomodoro',
-    component: Pomodoro
-  }
+    name: 'PomodoroPage',
+    component: PomodoroPage,
+  },
 ]
 
-// Criação e exportação do router
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
 })
 
 export default router
---
+++ src/views/PomodoroPage.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="flex min-h-screen bg-gray-50">
+    <!-- Sidebar -->
+    <Sidebar />
+
+    <!-- Conteúdo principal -->
+    <div class="flex-1 flex flex-col">
+      <!-- Header -->
+      <TheHeader />
+
+      <!-- Área do Pomodoro e ToDo -->
+      <main class="flex flex-col lg:flex-row gap-8 p-8">
+        <!-- Pomodoro -->
+        <section class="flex-1 bg-white rounded-2xl shadow-lg p-6">
+          <PomodoroTimer />
+        </section>
+
+        <!-- ToDo--> 
+        <section class="flex-1 rounded-2xl p-6">
+          <ToDo />
+        </section>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Sidebar from "../components/layout/Sidebar.vue";
+import TheHeader from "../components/layout/TheHeader.vue";
+import PomodoroTimer from "../components/pomodoro/PomodoroTimer.vue";
+import ToDo from "../components/task/ToDo.vue";
+</script>
+
+<style scoped>
+main {
+  transition: all 0.3s ease;
+}
+</style>
+
+
+
+
